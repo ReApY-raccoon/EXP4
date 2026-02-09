3f99fea840c2e883271b79cf645d9e8442e69860
Atharva Mondkar <atharva.mondkar.btech2028@atlasskilltech.university>
2026-02-09 12:24:30 +0530

    added comment

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import addition as a
 import subtraction as s
 import multiplication as m
 
+#calling functions
 print("Addition result = ", a.add(5,4))
 print("Subtraction result = ",s.sub(9,5))
 print("Multiplication result = ",m.mul(5,4))
