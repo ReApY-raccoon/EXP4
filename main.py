5f5b5ae850e43fac0f59233c5e6f287e6a784341
Atharva <mondkar.atharva1995@gmail.com>
2026-02-09 12:21:52 +0530

    Minor comment change

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#Calculator 
+#Calculator for addition subtraction and multiplication
+
+import addition as a
+import subtraction as s
+import multiplication as m
+
+print("Addition result = ", a.add(5,4))
+print("Subtraction result = ",s.sub(9,5))
+print("Multiplication result = ",m.mul(5,4))
