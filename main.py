c69a797b02fbcd1e574f18ec537a81bb8027e09c
Atharva <mondkar.atharva1995@gmail.com>
2026-02-09 12:53:14 +0530

    division

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 import addition as a
 import subtraction as s
 import multiplication as m
+import division as d
 
 #calling functions
 print("Addition result = ", a.add(5,4))
 print("Subtraction result = ",s.sub(9,5))
 print("Multiplication result = ",m.mul(5,4))
 #here call div function
+
